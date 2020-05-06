2faaaec0c8d6f5fa84547d68d5d3dbcedbefb552
Mauro Riva <lemariva@gmail.com>
2020-05-07 00:23:21 +0200

    initial commit

---
+++ micro_play.py
@@ -0,0 +1,45 @@
+# Copyright 2020 Mauro Riva - lemariva.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0#
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from machine import I2S
+from machine import Pin
+
+
+bck_pin = Pin(19)   # Bit clock output
+ws_pin = Pin(33)    # Word clock output
+sdout_pin = Pin(22) # Serial data output
+sdin_pin = Pin(23) # Serial data output
+
+audio_in = I2S(I2S.NUM0,                                 # create I2S peripheral to read audio
+               ws=ws_pin, sdin=sdin_pin,                 # 
+               standard=I2S.PHILIPS, mode=I2S.MASTER_PDW, # 
+               dataformat=I2S.B16,                       # 
+               channelformat=I2S.ONLY_RIGHT,
+               samplerate=44100, 
+               dmacount=16,dmalen=512)
+
+
+samples = bytearray(44100)
+
+num_bytes_read = audio_in.readinto(samples)
+
+audio_out = I2S(I2S.NUM1,                                  # create I2S peripheral to write audio
+                bck=bck_pin, ws=ws_pin, sdout=sdout_pin,    # sample data to an Adafruit I2S Amplifier
+                standard=I2S.PHILIPS, mode=I2S.MASTER_TX,  # breakout board, 
+                dataformat=I2S.B16,                        # based on MAX98357A device
+                channelformat=I2S.ONLY_RIGHT,
+                samplerate=44100, 
+                dmacount=16,dmalen=512)
+
+num_bytes_written = audio_out.write(samples) 
---
+++ play_wav.py
@@ -0,0 +1,54 @@
+#
+# This file is part of the MicroPython ESP32 project
+# 
+# The MIT License (MIT)
+#
+# Copyright (c) 2019 Mike Teachman
+# Copyright (c) 2020 Mauro Riva - lemariva.com
+# https://opensource.org/licenses/MIT
+# 
+# working with M5Stack ATOM Echo
+
+  
+from machine import I2S
+from machine import Pin
+
+SAMPLES_PER_SECOND = 44100
+
+bck_pin = Pin(19)   # Bit clock output
+ws_pin = Pin(33)    # Word clock output
+sdout_pin = Pin(22) # Serial data output
+sdin_pin = Pin(23) # Serial data output
+
+# channelformat settings:
+#    stereo WAV:  channelformat=I2S.RIGHT_LEFT
+#    mono WAV:    channelformat=I2S.ONLY_RIGHT
+audio_out = I2S(I2S.NUM1, bck=bck_pin, ws=ws_pin, sdout=sdout_pin, 
+              standard=I2S.PHILIPS, mode=I2S.MASTER_TX,
+              dataformat=I2S.B16, channelformat=I2S.ONLY_RIGHT,
+              samplerate=SAMPLES_PER_SECOND,
+              dmacount=8, dmalen=512)
+
+s = open('test-audio.wav','rb')
+s.seek(44) # advance to first byte of Data section in WAV file
+
+# continuously read audio samples from the WAV file 
+# and write them to an I2S DAC
+while True:
+    try:
+        audio_samples = bytearray(s.read(1024))
+        numwritten = 0
+        # end of WAV file?    
+        if len(audio_samples) == 0:
+            s.seek(44) # advance to first byte of Data section
+        else:
+            # loop until samples can be written to DMA
+            while numwritten == 0:
+                # return immediately when no DMA buffer is available (timeout=0)
+                numwritten = audio_out.write(audio_samples, timeout=0)
+                
+                # await - allow other coros to run   
+    except KeyboardInterrupt:  
+        s.close()
+        audio_out.deinit()
+        break
